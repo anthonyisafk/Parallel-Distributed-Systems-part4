cf74d6174c6b08f666b532407bb651b828db60f5
anthonyisafk <tonytonytony555@live.com>
2022-09-28 12:51:54 +0000

    Move source code into the src/ directory

--- util/cilk_helpers.h
+++
@@ -1,7 +0,0 @@
-#ifndef CILK_HELPERS_H
-#define CILK_HELPERS_H
-
-void cilk_axpy(const float* x, const float alpha, const int size, float* y);
-float cilk_sum(const float* x, const float alpha, const int size);
-
-#endif
--- util/seq_helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEQ_HELPERS_H
-#define SEQ_HELPERS_H
-
-void print_vector(const float* vec, const int size);
-void seq_axpy(const float* x, const float alpha, const int size, float* y);
-float seq_sum(const float* x, const float alpha, const int size);
-
-#endif
